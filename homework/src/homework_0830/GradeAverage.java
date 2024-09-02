8f3f4925b37d54c7f2fc406c44da482f3ad8189a
h-otwosion <106857969+h-otwosion@users.noreply.github.com>
2024-09-02 19:06:23 +0900

    Add files via upload

---
+++ homework/src/homework_0830/GradeAverage.java
@@ -0,0 +1,40 @@
+package practice01;
+import java.util.Scanner;
+
+public class GradeAverage {
+
+	public static void main(String[] args) {
+		int numStudent = getNumberKeyboard("학생 수 : ");
+		int[] scores = getScoresLine(numStudent);
+		printSumAverage(scores);
+	}
+	
+	
+	private static void printSumAverage(int[] scores) {
+		int haab = 0;
+		
+		for (int i = 0; i < scores.length; i++) {
+			haab += scores[i];
+		}
+		System.out.println(">>합계 : " + haab);
+		System.out.println(">>평균 : " + haab / scores.length);
+	}
+	
+	private static int[] getScoresLine(int std) {
+		Scanner sc = new Scanner(System.in);
+		
+		int[] scores = new int[std];
+		System.out.printf("%d 명의 성적 : ", std);
+		for (int i = 0; i < scores.length; i++) {
+			scores[i] = sc.nextInt();
+		}
+		return scores;
+	}
+	
+	private static int getNumberKeyboard(String str) {
+		System.out.print(str);
+		Scanner sc = new Scanner(System.in);
+		int numStd = sc.nextInt();
+		return numStd;
+	}
+}
